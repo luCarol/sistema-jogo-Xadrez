2b920a02bda48fe9b24d5633f973c844e3ecc7c5
Luana <lucarolp.deoli@gmail.com>
2020-01-16 19:52:30 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+/**
+ *
+ * @author Luana
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello world!");
+        
+    }
+    
+}
